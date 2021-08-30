c114d224072571218503634d1a1ea2812a67d1be
oshirok <switzerland@live.jp>
2021-08-29 18:06:18 -0700

    Change index redirection logic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MainComponent from "./MainComponent.jsx";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
-const mountPoint = document.createElement('div');
-mountPoint.id="#react_component";
+const mountPoint = document.createElement("div");
+mountPoint.id = "#react_component";
 document.body.appendChild(mountPoint);
 
 ReactDOM.render(
   <Router>
-    <Route exact path="/" component={MainComponent}>
-    </Route>
+    <Route exact path="*" component={MainComponent} />
   </Router>,
   mountPoint
 );
